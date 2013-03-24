7858762385e01a2bcba697d491820909766eb085
Andrey <hello.andrey@gmail.com>
2013-03-24 21:16:29 +0100

    initial sources

---
+++ convert.js
@@ -0,0 +1,25 @@
+function convert(object, base){
+	var root = object;
+	var parent = root;
+	var children = parent.children;
+	var classes = [];
+	var createClasses = function(node){
+		if(node.children.length>0){
+			var children = node.children;
+			for(var c in children){
+				var child = children[c];
+				classes.push('<owl:Class rdf:about="' + base + '#' + child.title + '"><rdfs:subClassOf rdf:resource="' + base + '#' + parent.title +  '"/></owl:Class>');
+				createClasses(child);	
+			}
+			
+		}
+	}
+	for(var c in children){
+		var child = children[c];
+		classes.push('<owl:Class rdf:about="' + base + '#' + child.title + '"><rdfs:subClassOf rdf:resource="http://www.w3.org/2002/07/owl#Thing"/></owl:Class>');
+		createClasses(child);
+	}
+
+	return classes;
+}
+
---
+++ mindmap2owl.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('#jsoninput').append(JSON.stringify(tree)); 
+	$('#btn_convert').click(function(){
+		var jsoninput = $('#jsoninput').val();
+		var base = $('#base').val();
+		var object = {};
+		try{
+			object = JSON.parse(jsoninput);
+			object = (object.children)? object: ((object.root)? object.root: object);
+			var classes = convert(object, base);
+			$('#xmlout').append(classes.toString().replace(/</g,'&lt;').replace(/>/g,'&gt;'));
+		}
+		catch(e){ console.log('Failed to parse JSON: '+e);}
+	})
+
+});
+
