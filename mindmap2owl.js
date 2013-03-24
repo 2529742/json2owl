712c3f79fd37aff725edbcd900003f1d11d551bc
Andrey <hello.andrey@gmail.com>
2013-03-24 21:27:05 +0100

    classes array to sting fix

--- mindmap2owl.js
+++ mindmap2owl.js
@@ -7,7 +7,7 @@ $(function(){
 			object = JSON.parse(jsoninput);
 			object = (object.children)? object: ((object.root)? object.root: object);
 			var classes = convert(object, base);
-			$('#xmlout').append(classes.toString().replace(/</g,'&lt;').replace(/>/g,'&gt;'));
+			$('#xmlout').append(classes.join("").replace(/</g,'&lt;').replace(/>/g,'&gt;'));
 		}
 		catch(e){ console.log('Failed to parse JSON: '+e);}
 	})
